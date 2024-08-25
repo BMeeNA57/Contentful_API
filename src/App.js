5f2d5b53bf832ee7728318f82f30eb6b4848b8d2
Meena <59247187+BMeeNA57@users.noreply.github.com>
2024-08-25 12:07:07 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import './App.css'; 
 import axios from 'axios';
 
 function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    axios.get('https://preview.contentful.com/spaces/4q3xv9m8zuzc?access_token=USE AUTH TOKEN') // Replace with your API endpoint
+    axios.get('https://preview.contentful.com/spaces/4q3xv9m8zuzc?access_token=VYlVhC775F4AWYbYJWff40y7_PNIKUc3IDtSnSoAft8') // Replace with your API endpoint
       .then(response => {
         setData(response.data);
       })
@@ -15,11 +16,35 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>My React App</h1>
+    <div className="App" className="container">
+      <h1>Contentful Preview API</h1>
       {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading data...</p>}
     </div>
   );
 }
 
+function App2() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://cdn.contentful.com/spaces/4q3xv9m8zuzc?access_token=VYlVhC775F4AWYbYJWff40y7_PNIKUc3IDtSnSoAft8') // Replace with your API endpoint
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the data!", error);
+      });
+  }, []);
+
+  return (
+    <div className="App2" className="container">
+      <h1>Contentful Delivery API</h1>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading data...</p>}
+    </div>
+  );
+}
+
+
 export default App;
+
+
